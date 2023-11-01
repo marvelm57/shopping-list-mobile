e29f25a39384e9c81ae4523cbce91ed11e13daaa
“marvelm57” <vivalamarvel@gmail.com>
2023-11-01 15:09:58 +0700

    fix menu.dart

--- lib/menu.dart
+++ lib/menu.dart
@@ -12,58 +12,58 @@ class MyHomePage extends StatelessWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final List<ShopItem> items = [
-    ShopItem("Lihat Produk", Icons.checklist),
-    ShopItem("Tambah Produk", Icons.add_shopping_cart),
-    ShopItem("Logout", Icons.logout),
+  final List<ShopItem> items = const [
+    const ShopItem("Lihat Produk", Icons.checklist),
+    const ShopItem("Tambah Produk", Icons.add_shopping_cart),
+    const ShopItem("Logout", Icons.logout),
   ];
 
   @override
     Widget build(BuildContext context) {
         return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Shopping List',
-        ),
-      ),
-      body: SingleChildScrollView(
-        // Widget wrapper yang dapat discroll
-        child: Padding(
-          padding: const EdgeInsets.all(10.0), // Set padding dari halaman
-          child: Column(
-            // Widget untuk menampilkan children secara vertikal
-            children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                // Widget Text untuk menampilkan tulisan dengan alignment center dan style yang sesuai
-                child: Text(
-                  'PBP Shop', // Text yang menandakan toko
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              // Grid layout
-              GridView.count(
-                // Container pada card kita.
-                primary: true,
-                padding: const EdgeInsets.all(20),
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                crossAxisCount: 3,
-                shrinkWrap: true,
-                children: items.map((ShopItem item) {
-                  // Iterasi untuk setiap item
-                  return ShopCard(item);
-                }).toList(),
-              ),
-            ],
+          appBar: AppBar(
+            title: const Text(
+              'Shopping List',
+            ),
           ),
-        ),
-      ),
-    );
+          body: SingleChildScrollView(
+            // Widget wrapper yang dapat discroll
+            child: Padding(
+              padding: const EdgeInsets.all(10.0), // Set padding dari halaman
+              child: Column(
+                // Widget untuk menampilkan children secara vertikal
+                children: <Widget>[
+                  const Padding(
+                    padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                    // Widget Text untuk menampilkan tulisan dengan alignment center dan style yang sesuai
+                    child: Text(
+                      'PBP Shop', // Text yang menandakan toko
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  // Grid layout
+                  GridView.count(
+                    // Container pada card kita.
+                    primary: true,
+                    padding: const EdgeInsets.all(20),
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    crossAxisCount: 3,
+                    shrinkWrap: true,
+                    children: items.map((ShopItem item) {
+                      // Iterasi untuk setiap item
+                      return ShopCard(item);
+                    }).toList(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
     }
 }
 
@@ -71,7 +71,7 @@ class ShopItem {
   final String name;
   final IconData icon;
 
-  ShopItem(this.name, this.icon);
+  const ShopItem(this.name, this.icon);
 }
 
 class ShopCard extends StatelessWidget {
